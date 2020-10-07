affa16017165a0b0901ea5cb8168a8bb65ff4b14
Ryan Gasparini <ryan@rxgx.com>
2020-10-07 02:57:57 -0700

    chore: update eslint blog

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
   plugins: ['react'],
   settings: {
     react: {
-      version: 'detect'
+      version: 'detect',
     },
   },
   rules: {
     'prettier/prettier': ['error', { singleQuote: true }],
     'react/prop-types': ['warn'],
-    'react/jsx-no-comment-textnodes': ['warn']
-  }
+    'react/jsx-no-comment-textnodes': ['warn'],
+  },
 };
